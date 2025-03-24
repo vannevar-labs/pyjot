1279365a7f9afa3afe451a6fbff5eb99ae723fb8
Colin Putney <colin.putney@vannevarlabs.com>
2025-03-24 10:19:10 -0600

    Add support for instrumenting via decorators

---
+++ jot/decorators.py
@@ -0,0 +1,91 @@
+import asyncio
+import functools
+import inspect
+from copy import copy
+
+from . import facade as _facade
+
+
+def instrument(*dtags, **stags):
+    if len(dtags) == 1 and callable(dtags[0]):
+        # Uncalled decorator
+        return make_decorator([], {})(dtags[0])
+
+    # Called decorator
+    return make_decorator(dtags, stags)
+
+
+def make_decorator(dtags, stags):
+    def decorator(func):
+        if inspect.iscoroutinefunction(func):
+            return wrap_async(func, dtags, stags)
+
+        return wrap_sync(func, dtags, stags)
+
+    return decorator
+
+
+def wrap_async(func, dynamic_tag_names, static_tags):
+    name = func.__name__
+
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        tags = extract_tags(dynamic_tag_names, static_tags, kwargs)
+        child = _facade.active_meter.span(name, **tags)
+        parent = _facade._swap_active(child)
+        try:
+            coro = func(*args, **kwargs)
+            child.start()
+            try:
+                future = coro.send(None)
+                _facade._swap_active(parent)
+                while True:
+                    done, pending = await asyncio.wait([future])
+                    value = done.pop().result()
+
+                    _facade._swap_active(child)
+                    future = coro.send(value)
+                    _facade._swap_active(parent)
+
+            except StopIteration as e:
+                return e.value
+
+        except Exception as e:
+            child.error(f"Error during {name}", e)
+            raise
+
+        finally:
+            _facade._swap_active(parent)
+            child.finish()
+
+    return wrapper
+
+
+def wrap_sync(func, dynamic_tag_names, static_tags):
+    name = func.__name__
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        tags = extract_tags(dynamic_tag_names, static_tags, kwargs)
+        child = _facade.active_meter.start(name, **tags)
+        parent = _facade._swap_active(child)
+        try:
+            return func(*args, **kwargs)
+
+        except Exception as e:
+            child.error(f"Error during {name}", e)
+            raise
+
+        finally:
+            _facade._swap_active(parent)
+            child.finish()
+
+    return wrapper
+
+
+def extract_tags(dynamic_tag_names, static_tags, keyword_args):
+    tags = copy(static_tags)
+    for tag_name in dynamic_tag_names:
+        if tag_name in keyword_args:
+            tags[tag_name] = keyword_args.pop(tag_name)
+    return tags
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import inspect
+
 import pytest
 
 
@@ -34,3 +36,75 @@ def assert_child_tags_are_correct(assert_tags_are_correct):
 @pytest.fixture
 def child_tags(jot, tags):
     return {**jot.tags, **tags}
+
+
+def is_test_function(name):
+    if name in ["jot", "args", "select"]:
+        return False
+    if name.startswith("_"):
+        return False
+    return True
+
+
+def decorated_functions():
+    from . import decorated_fn as fn
+
+    return [getattr(fn, name) for name in dir(fn) if is_test_function(name)]
+
+
+def pytest_generate_tests(metafunc):
+    if "func" in metafunc.fixturenames:
+        fns = decorated_functions()
+
+        # filter by function type
+        if inspect.iscoroutinefunction(metafunc.function):
+            fns = [fn for fn in fns if inspect.iscoroutinefunction(fn)]
+        else:
+            fns = [fn for fn in fns if not inspect.iscoroutinefunction(fn)]
+
+        # filter by selectors marks, if any
+        for markinfo in metafunc.definition.iter_markers():
+            if markinfo.name == "select":
+                test_selectors = set(markinfo.args)
+                _fns = []
+                for fn in fns:
+                    fn_selectors = getattr(fn, "_jot_selectors", set())
+                    if test_selectors.issubset(fn_selectors):
+                        _fns.append(fn)
+                fns = _fns
+            if markinfo.name == "reject":
+                test_selectors = set(markinfo.args)
+                _fns = []
+                for fn in fns:
+                    fn_selectors = getattr(fn, "_jot_selectors", set())
+                    if not test_selectors.intersection(fn_selectors):
+                        _fns.append(fn)
+                fns = _fns
+
+        # parametrize by args and kwargs, if the function requests both
+        if "args" in metafunc.fixturenames and "kwargs" in metafunc.fixturenames:
+            params = []
+            for fn in fns:
+                for args, kwargs in getattr(fn, "_jot_args", []):
+                    params.append((fn, args, kwargs))
+            metafunc.parametrize(["func", "args", "kwargs"], params)
+
+        # parametrize by args only, if the function requests it
+        elif "args" in metafunc.fixturenames:
+            params = []
+            for fn in fns:
+                for args, _ in getattr(fn, "_jot_args", []):
+                    params.append((fn, args))
+            metafunc.parametrize(["func", "args"], params)
+
+        # parametrize by kwargs only, if the function requests it
+        elif "kwargs" in metafunc.fixturenames:
+            params = []
+            for fn in fns:
+                for _, kwargs in getattr(fn, "_jot_args", []):
+                    params.append((fn, kwargs))
+            metafunc.parametrize(["func", "kwargs"], params)
+
+        # parametrize by function only
+        else:
+            metafunc.parametrize("func", fns)
